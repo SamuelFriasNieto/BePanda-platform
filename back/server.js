78b5b23cb7cefd8a334b76e2288c8a21086ac7c2
Samuel <sae.nieto@gmail.com>
2024-09-10 10:59:25 +0200

    first commit

---
+++ back/server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+const port= 3001
+app.use(express.json());
+
+app.get('/', (req,res) => { res.send( 'Hola')})
+
+app.listen(port, () =>{console.log('funciona')})

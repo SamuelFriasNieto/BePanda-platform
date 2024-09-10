72817823413475d7458ef1c251b5d23485a92512
Samuel Frias Nieto <sae.nieto@gmail.com>
2024-09-10 14:57:57 +0200

    vercel config 4

--- back/server.js
+++ back/server.js
@@ -1,16 +1,12 @@
-import express from 'express';
-
-
-// api/index.js
+// server.cjs
+const express = require('express');
 const app = express();
-import serverless from 'serverless-http'
+const serverless = require('serverless-http');
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello from Express on Vercel!');
 });
-
-// Puedes agregar más rutas aquí
 
 module.exports.handler = serverless(app);
